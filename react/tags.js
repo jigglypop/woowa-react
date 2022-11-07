d8cc86cad351812d3bfda94521b276eb20a0e0a3
jigglypop <ydh2244@gmail.com>
2022-11-07 23:00:53 +0900

    1107

--- react/tags.js
+++ react/tags.js
@@ -1,13 +1,13 @@
 // 인자 찾기
 export const getParams = (text) => {
-  const componentTag = ":.*?=\".*?\"";
+  const componentTag = ':.*?=".*?"';
   const regex = new RegExp(componentTag, "g");
-  const datas = text.match(regex) || []
-  let _datas = {}
-  datas.map(item => {
-    const [name, value] = item.replace(/(\"|:)/g, "").split("=")
-    _datas[name] = value
-  })
+  const datas = text.match(regex) || [];
+  let _datas = {};
+  datas.map((item) => {
+    const [name, value] = item.replace(/(\"|:)/g, "").split("=");
+    _datas[name] = value;
+  });
   return _datas;
 };
 // 컴포넌트 인지
@@ -15,13 +15,15 @@ export const isComponent = (text) => {
   const openTag = "(<.*/>)";
   const componentSetTag = `${openTag}`;
   const jsxRegex = new RegExp(componentSetTag, "g");
-  const jsxs = text.match(jsxRegex)
-  const jsxDatas = jsxs ? jsxs.map((jsx) => {
-    return {
-      'jsx': jsx,
-      "params": getParams(jsx)
-    }
-  }) : null
+  const jsxs = text.match(jsxRegex);
+  const jsxDatas = jsxs
+    ? jsxs.map((jsx) => {
+        return {
+          jsx: jsx,
+          params: getParams(jsx),
+        };
+      })
+    : null;
   return jsxDatas;
 };
 // 태그 찾기
